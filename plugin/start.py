f50d678eb26716b1ee359273352d52081932230d
itz-dude <its.kaydem@gmail.com>
2022-07-03 16:03:11 +0530

    Update start.py

--- plugin/start.py
+++ plugin/start.py
@@ -2,4 +2,4 @@ from pyrogram import *
 from pyrogram.types import *
 
 def start(client, message):
-    message.reply_text("""**/search <hentai name> to search**\n Powered By @MetaVoid""", parse_mode="markdown")
+    message.reply_text("""**/search <hentai name> to search**\n Powered By @MangaX_updates [🥳](https://telegra.ph/file/8dd8cc843ae45ebc5b4b5.png)""", parse_mode="markdown")
